1bd49f8b401dd5b21b9625d8c2006bcbff30ea31
wqy <weiqy@kingnet.com>
2016-10-24 15:24:06 +0800

    button

---
+++ phone/button/handup_button.cpp
@@ -0,0 +1,27 @@
+#include "handup_button.h"
+
+void HandUpButton::OnDown()
+{
+    BrailleSquare::OnDown();
+}
+
+// 按下抬起
+void HandUpButton::OnDownUp()
+{
+    BrailleSquare::OnDownUp();
+}
+
+// 抬起
+void HandUpButton::OnUp()
+{
+    BrailleSquare::OnUp();
+}
+
+// 抬起按下
+void HandUpButton::OnUpDown()
+{
+    BrailleSquare::OnUpDown();
+}
+
+
+
---
+++ phone/button/handup_button.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "braille_square.h"
+
+/*
+ * =====================================================================================
+ *        Class:  HandUpButton
+ *  Description:  
+ * =====================================================================================
+ */
+class HandUpButton : public BrailleSquare
+{
+    public:
+        /* ====================  LIFECYCLE     ======================================= */
+        HandUpButton ()
+        {
+        }
+
+        // 按下
+        virtual void OnDown();
+
+        // 按下抬起
+        virtual void OnDownUp();
+
+        // 抬起
+        virtual void OnUp();
+
+        // 抬起按下
+        virtual void OnUpDown();
+
+    protected:
+
+}; /* -----  end of class HandUpButton  ----- */
+
